5d8c800eb729924eb5c85e70fae049907ce79ee9
gustavorodriguez92 <gustavo.rodriguez@wizeline.com>
2025-03-19 15:44:21 -0500

    Initial Commit

---
+++ HackathonTests/HackathonTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonTests.swift
+//  HackathonTests
+//
+//  Created by Gustavo Rodriguez on 19/03/25.
+//
+
+import Testing
+@testable import Hackathon
+
+struct HackathonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
